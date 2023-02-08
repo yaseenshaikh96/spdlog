86a822ff7f8b1c45fa21f13c002e032e1a758c45
Yaseen <shaikhyaseen6496@gmail.com>
2023-02-08 14:49:20 +0530

    modified project file

--- premake5.lua
+++ premake5.lua
@@ -2,3 +2,22 @@ project "spdlog"
 	kind "None"
 	language "C++"
 	cppdialect "C++20"
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp"
+	}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
