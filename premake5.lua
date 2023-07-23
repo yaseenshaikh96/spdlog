3026aa93ac2104be32f656e4f2d8e80a71ca76dd
Yaseen <shaikhyaseen6496@gmail.com>
2023-07-23 23:55:45 +0530

    minor edits to build file

--- premake5.lua
+++ premake5.lua
@@ -3,6 +3,9 @@ project "spdlog"
 	language "C++"
 	cppdialect "C++20"
 
+	targetdir ( "%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ( "%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
 	files
 	{
 		"include/**.h",
