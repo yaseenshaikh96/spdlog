8db5b0f5ffaf30a329653278a6f0c517f82cc3d9
Yaseen <shaikhyaseen6496@gmail.com>
2023-07-26 23:21:29 +0530

    new utput dirs

--- premake5.lua
+++ premake5.lua
@@ -3,8 +3,9 @@ project "spdlog"
 	language "C++"
 	cppdialect "C++20"
 
-	targetdir ( "%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
-	objdir ( "%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+    	targetdir ( "%{wks.location}/bin/" .. outputdir)
+    	objdir ( "%{wks.location}/bin-int/" .. outputdir)
+
 
 	files
 	{
